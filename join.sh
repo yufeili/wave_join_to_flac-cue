644d0ddf152e2369269d516e07746dfd287f1aae
yufeili <yufeili.email@gmail.com>
2019-04-20 12:47:52 -0400

    修改文件夹位置

--- join.sh
+++ join.sh
@@ -2,10 +2,10 @@
 #将多个.wav文件合成一个.wav并转码为.flac加.cue
 rm -f CDImage.flac
 rm -f CDImage.cue
-rm -f /dev/shm/out.wav
-python /home/liyf/ftp/wave_join.py *.wav -o /dev/shm/out.wav
-shntool conv -o flac /dev/shm/out.wav -d ./ -O always
-mv /dev/shm/out.flac CDImage.flac
-rm -f /dev/shm/out.wav
-rm -f /dev/shm/out.flac
+rm -f out.wav
+python ./wave_join.py *.wav -o out.wav
+shntool conv -o flac out.wav -d ./ -O always
+mv out.flac CDImage.flac
+rm -f out.wav
+rm -f out.flac
 exit
